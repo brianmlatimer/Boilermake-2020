ca5f2c1988fa0ee4be3aaf1bf048c86714b0973d
Matthew Wen <mattwen2018@gmail.com>
2020-01-25 04:07:05 -0600

    Made a lot of progress

--- android/app/src/main/java/com/boilermake/mwen/MainActivity.java
+++ android/app/src/main/java/com/boilermake/mwen/MainActivity.java
@@ -1,14 +1,28 @@
 package com.boilermake.mwen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] items = {"item 0", "item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        RecyclerView rv = findViewById(R.id.recyclerview);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new adapter(this, items));
+
+//        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.main_tool_bar);
+//        setSupportActionBar(toolbar);
+
     }
 }
